a4e23e8ec22fb89aa43a09dd2fe2e8dd0403e685
Prelang Builder <builder@prelang.com>
2016-01-10 01:38:48 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Preference' to allow for create, update

--- app/admin/preference.rb
+++ app/admin/preference.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Preference do
+  permit_params :position, :frequency
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
