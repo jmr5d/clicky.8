5f0da484bd1a79070d4ff3d93c35d0bd2a3d0f6f
josiahredding <josiahredding@gmail.com>
2016-01-13 14:35:37 -0600

    fixed model nested attrib error in measurements

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -2,7 +2,7 @@ class Measurement < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :measure_name
 	belongs_to :measure_unit
-	has_one :measure_unit, :through => :measure_name
+	# has_one :measure_unit, :through => :measure_name
 	accepts_nested_attributes_for :measure_name
 	accepts_nested_attributes_for :measure_unit
 	acts_as_taggable
