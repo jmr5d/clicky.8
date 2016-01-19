b1b3ff006974df50551cca1a824baf0763a49176
josiahredding <josiahredding@gmail.com>
2016-01-19 08:35:04 -0600

    fixed :login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_sanitized_params, if: :devise_controller?
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
   #->Prelang (user_login:devise)
   # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit() }
+  #   devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :remember_me,
+  #     :birthdate, :gender, :height, :first_name, :last_name) }
   #   devise_parameter_sanitizer.for(:sign_in)        { |u| u.permit(:login, :username, :email, :password, :remember_me) }
   #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password,
   #     :birthdate, :gender, :height, :first_name, :last_name) }
@@ -45,6 +47,10 @@ class ApplicationController < ActionController::Base
       user_params.permit(:username, :email, :password, :password_confirmation, :current_password,
       :birthdate, :gender, :height, :first_name, :last_name)
     end
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:login, :username, :email, :password, :remember_me)
+    end
+
   end
 
 end
